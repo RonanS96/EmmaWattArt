4699673d517d23eaa342be98d1d2aea65864b811
RonanS96 <rs6@hw.ac.uk>
2018-01-09 15:17:30 +0000

    Email may be working now with link back to site.

--- PHP/email.php
+++ PHP/email.php
@@ -10,14 +10,19 @@
 
 <?php
   $email_from     = 'Emma Watt Art';
-  $email_subject  = "New message from Emma Watt Art";
+  $email_subject  = "New message from Emma Watt art";
   $email_body     = "You have recieved a new message from " . $name . ":\n" . $message . ".\n"
 ?>
 
 <?php
   $to = "ronan1874@aol.co.uk";
   $headers = "From: $email_from \r\n";
-  $headers = "Reply-To: $sender \r\n";
+  $headers .= "Reply-To: $sender \r\n";
 
   mail($to,$email_subject,$email_body,$headers);
+?>
+
+<?php
+  echo "Your email has been sent!\n";
+  echo "<a href=\"../about.html\">Return to site</a>";
 ?>
